c2da69049dc6171b38144aa1762e378f34ebb583
san-1122 <sanhathai212@gmail.com>
2024-04-26 14:22:13 +0000

    update fig bug

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDB = await mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connect(connectMongoDB);
+// mongoose.connect(connectMongoDB);
 
 const connectionDatabase = mongoose.connection;
 
 export default connectMongoDB;
 export { connectionDatabase };
-    
